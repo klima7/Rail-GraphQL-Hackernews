2300c65c8db64e2e041091585656f3cc86e0a643
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-10-20 10:46:37 +0200

    Add mutation for adding users

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,5 +1,5 @@
 module Mutations
-  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+  class BaseMutation < GraphQL::Schema::Mutation
     null false
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_link, mutation: Mutations::CreateLink
     field :destroy_link, mutation: Mutations::DestroyLink
     field :update_link, mutation: Mutations::UpdateLink
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
