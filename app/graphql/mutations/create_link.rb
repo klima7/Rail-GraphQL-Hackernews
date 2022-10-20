3c8b79067a548968d7fa645ac223a17fb65ebc90
Lukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-10-20 11:45:43 +0200

    Add author automatically to links

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -6,9 +6,12 @@ module Mutations
     type Types::LinkType
 
     def resolve(description: nil, url: nil)
+      pp context[:session][:token]
+      pp context[:current_user]
       Link.create!(
         description: description,
         url: url,
+        user: context[:current_user]
       )
     end
   end
